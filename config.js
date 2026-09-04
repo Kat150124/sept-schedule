cc08e1b5ee0b554bcce9af24e9d9b93560a78034
Kat <kat150124@gmail.com>
2026-09-04 14:04:55 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * 共用設定：兩個頁面（index.html 的課表編輯 + notes.html 的執行筆記）
+ * 都讀這個檔案裡的網址，只要改這裡一個地方就好。
+ *
+ * 部署好 Apps Script 後，把網址貼在下面的引號中間。
+ * 格式類似：https://script.google.com/macros/s/xxxxxxxx/exec
+ */
+const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
